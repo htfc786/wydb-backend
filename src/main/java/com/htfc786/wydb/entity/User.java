e06a84fd8228b65d8cf85aa7d31048287cd54963
htfc786 <2721672639@qq.com>
2024-07-20 15:43:45 +0800

    整合mybatis测试

---
+++ src/main/java/com/htfc786/wydb/entity/User.java
@@ -0,0 +1,14 @@
+package com.htfc786.wydb.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String nickname;
+
+    private String password;
+}
