5a3ebac94fe9e22b15d441f17301ab26561f2235
htfc786 <2721672639@qq.com>
2024-07-20 11:31:42 +0800

    初始化项目

---
+++ src/main/java/com/htfc786/wydb/WydbApplication.java
@@ -0,0 +1,11 @@
+package com.htfc786.wydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WydbApplication {
+    public static void main( String[] args ){
+        SpringApplication.run(WydbApplication.class, args);
+    }
+}
---
+++ src/test/java/com/htfc786/wydb/WydbApplicationTest.java
@@ -0,0 +1,11 @@
+package com.htfc786.wydb;
+
+/**
+ * Unit test for simple App.
+ */
+public class WydbApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+}
